f2d5435542a745a4a0a1228a717a9194d9767c38
FaithKMuasya <loshyfaith@gmail.com>
2025-04-27 21:20:37 +0300

    navbar, hero-section created & designed

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -3,3 +3,11 @@
 //function showAlert() {
     //alert("Hello, this is an alert!");
 //}
+
+const menuToggle = document.querySelector('.menu-toggle'); // Hamburger icon
+const navLinks = document.querySelector('.nav-links'); // Navigation list
+
+menuToggle.addEventListener('click', () => {
+    // Toggle the "active" class to show or hide the menu
+    navLinks.classList.toggle('active');
+});
